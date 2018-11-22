60806e0545d3841e17653bc4ff98ea31416d5f47
Alan Vieceli <alansvieceli@gmail.com>
2018-11-22 13:39:27 -0200

    Integração com Travis CI

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+
+class CoreTests(APITestCase):
+
+    url_default = '/'
+
+    def test_get_page_status_code(self):
+        response = self.client.get(self.url_default)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
